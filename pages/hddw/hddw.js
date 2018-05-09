55088a0cd56c47db2ba5e8936d3a200c9faa2cbc
17868804701 <742210203@qq.com>
2018-05-09 18:15:50 +0800

    修改签退判断

--- pages/hddw/hddw.js
+++ pages/hddw/hddw.js
@@ -18,6 +18,7 @@ Page({
   * 生命周期函数--监听页面加载
   */
  onLoad: function (options) {
+
   let that = this;
   // 加载活动定位列表
   wx.request({
@@ -28,6 +29,7 @@ Page({
     "Content-Type": "application/json"
    },
    success: function (res) {
+     console.log(res)
     if (res.data.resultCode == '-9999') {
      wx.showModal({
       title: '提示',
@@ -168,66 +170,83 @@ Page({
   })
  },
  fqqt: function (e) {
-  let eventId = e.currentTarget.dataset.eventid;
-  console.log(eventId)
-  wx.request({
-   url: `${getApp().data.url}business/secClassRoom/event/eventEnd`,
-   method: 'POST',
-   data: { "params": { "userName": getApp().data.userName, "sessionId": getApp().data.sessionId, "eventId": eventId } },
-   header: {
-    "Content-Type": "application/json"
-   },
-   success: function (res) {
-    console.log(res.data)
-    if (res.data.resultCode == '-9999') {
-     wx.showModal({
-      title: '提示',
-      content: '网络异常',
-      success: function (res) {
-       if (res.confirm) {
-        console.log('用户点击确定')
-       }
-      }
-     })
-    } else if (res.data.resultCode == '-1111') {
-     wx.showModal({
-      title: '登录提示',
-      content: '登录超时，跳转到主页...',
-      success: function (res) {
-       if (res.confirm) {
-        wx.redirectTo({
-         url: '../login/login',
-        })
-       } else if (res.cancel) {
-        wx.redirectTo({
-         url: '../login/login',
-        })
-       }
-      }
-     })
-    } else {
-     wx.showModal({
-      title: '签退提示',
-      content: '活动已经结束，请学生进行活动签退',
-      success: function (res) {
-       if (res.confirm) {
-        console.log('用户点击确定')
-        wx.navigateBack({
-         url: '../sh/sh',
-        })
-       }
-      }
-     })
-    }
-   }
-   ,
-   fail: function (err) {
-    console.log(err)
-   },
-   complete: function (data) {
 
-   }
-  })
+   var myDate = new Date();
+   var time = myDate.getTime()
+   console.log(time)//当前时间的时间戳
+
+
+  let eventId = e.currentTarget.dataset.eventid;
+  let startTime = e.currentTarget.dataset.starttime;
+  console.log(eventId)
+  console.log(startTime)
+  var date = new Date(startTime);
+  var time1 = date.getTime();//活动开始时间戳
+  if(time<time1){
+    wx.showModal({
+      title: '提示',
+      content: '活动还没结束，不能进行签退',
+    })
+  }else{
+    wx.request({
+      url: `${getApp().data.url}business/secClassRoom/event/eventEnd`,
+      method: 'POST',
+      data: { "params": { "userName": getApp().data.userName, "sessionId": getApp().data.sessionId, "eventId": eventId } },
+      header: {
+        "Content-Type": "application/json"
+      },
+      success: function (res) {
+        console.log(res.data)
+        if (res.data.resultCode == '-9999') {
+          wx.showModal({
+            title: '提示',
+            content: '网络异常',
+            success: function (res) {
+              if (res.confirm) {
+                console.log('用户点击确定')
+              }
+            }
+          })
+        } else if (res.data.resultCode == '-1111') {
+          wx.showModal({
+            title: '登录提示',
+            content: '登录超时，跳转到主页...',
+            success: function (res) {
+              if (res.confirm) {
+                wx.redirectTo({
+                  url: '../login/login',
+                })
+              } else if (res.cancel) {
+                wx.redirectTo({
+                  url: '../login/login',
+                })
+              }
+            }
+          })
+        } else {
+          wx.showModal({
+            title: '签退提示',
+            content: '活动已经结束，请学生进行活动签退',
+            success: function (res) {
+              if (res.confirm) {
+                console.log('用户点击确定')
+                wx.navigateBack({
+                  url: '../sh/sh',
+                })
+              }
+            }
+          })
+        }
+      }
+      ,
+      fail: function (err) {
+        console.log(err)
+      },
+      complete: function (data) {
+
+      }
+    })
+  }
  },
  /**
   * 生命周期函数--监听页面初次渲染完成
